d71461eb7d87bd2f1ea98ab0d3b1212008418cfe
Kim <ninehundred@outlook.kr>
2015-11-03 19:04:03 +0900

    1103별찍기

--- KimNinehundred/Sooltudy/app/src/main/java/com/sku/sooltudy/MainActivity.java
+++ KimNinehundred/Sooltudy/app/src/main/java/com/sku/sooltudy/MainActivity.java
@@ -1,10 +1,12 @@
 package com.sku.sooltudy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -15,18 +17,29 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvMain = (TextView)findViewById(R.id.textView);
-
-        tvMain.setText("Hello Sooltudy!");
-
-        tvMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvMain.setText("Hello alcholStudy");
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(),SecondActivity.class);
+                startActivity(it);
             }
+
         });
-    }
 
 
 
-}
+////        tvMain = (TextView)findViewById(R.id.textView);
+////
+////        tvMain.setText("Hello Sooltudy!");
+////
+////        tvMain.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                tvMain.setText("Hello alcholStudy");
+//            }
+//        });
+//    }
+
+
+
+}}
